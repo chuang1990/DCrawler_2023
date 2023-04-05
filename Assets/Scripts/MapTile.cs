58a8719aa48539fdb32f448d61c6438446c8400b
Brian Burns <brian.b5829@gmail.com>
2023-04-05 20:45:51 +1000

    Added resource bars to HUD and battle screens

--- Assets/Scripts/MapTile.cs
+++ Assets/Scripts/MapTile.cs
@@ -13,11 +13,11 @@ public class MapTile : TileBase
 	public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
 	{
 		tileData.gameObject = Prefab;
+		if (Prefab == null) return;
+		
+		var texture = AssetPreview.GetAssetPreview(Prefab);
+		if (texture == null) return;
 
-		if (Prefab != null)
-		{
-			var texture = AssetPreview.GetAssetPreview(Prefab);
-			tileData.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
-		}
+		tileData.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
 	}
 }
